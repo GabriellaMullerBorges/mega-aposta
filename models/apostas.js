9feaeb4828e09c0babd2d2cb7304217f262bb5b6
GabriellaMullerBorges <gabimullerborges@gmail.com>
2024-03-20 20:29:03 -0300

    feat: adição de apostas

--- models/apostas.js
+++ models/apostas.js
@@ -10,23 +10,25 @@ const Apostas = sequelize.define('Apostas', {
   },
   numeros: {
     type: DataTypes.STRING, 
-    allowNull: false,
-    get() {
-      return this.getDataValue('numeros').split(','); // Convertendo a string de volta para array
-    },
-    set(val) {
-       this.setDataValue('numeros', val.join(',')); // Convertendo array para string para armazenar
-    }
+    allowNull: false
   },
   apostadorCPF: { // usando o CPF como chave
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING,
     references: {
       model: Apostadores, 
       key: 'cpf', 
     }
+  }, 
+  createdAt: {
+    type: DataTypes.STRING(200),
+    defaultValue: 'hoje'
+  },
+  updatedAt: {
+    type: DataTypes.STRING(200),
+    defaultValue: 'hoje'
   }
 }, {
-  timestamps: true,
+  timestamps:false,
 });
 
 module.exports = Apostas;
--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,6 @@ router.post('/registrar', async (req, res) => {
   const { nome, cpf , createdAt,  updatedAt} = req.body;
 
   if (!nome || !cpf) {
-    console.log('AAA')
     return res.status(400).send('Nome e CPF são obrigatórios.');
   }
 
@@ -50,21 +49,43 @@ req.session.save(err => {
   });
 });
 
-  router.get('/home', (req, res) => {
-    console.log('TESTANDO de novo', req.session);
-    if (req.session.userCPF && req.session.userName) {
-      console.log("CPF do usuário na sessão:", req.session.userCPF);
-      console.log("Nome do usuário na sessão:", req.session.userName);
-      res.render('index', {
-        userCPF: req.session.userCPF,
-        userName: req.session.userName
-      });
-    } else {
-      res.render('index', {
-        userCPF: '',
-        userName: 'Usuário não identificado'
-      });
+router.get('/home', (req, res) => {
+  console.log('TESTANDO de novo', req.session);
+  if (req.session.userCPF && req.session.userName) {
+    console.log("CPF do usuário na sessão:", req.session.userCPF);
+    console.log("Nome do usuário na sessão:", req.session.userName);
+    res.render('index', {
+      userCPF: req.session.userCPF,
+      userName: req.session.userName
+    });
+  } else {
+    res.render('index', {
+      userCPF: '',
+      userName: 'Usuário não identificado'
+    });
+  }
+});
+
+// Rota para registrar uma nova aposta
+router.post('/registrar-aposta', async function(req, res, next) {
+  try {
+    const { userName, userCPF } = req.body;
+    let numeros = req.body.numeros;
+
+    // Se 'numeros' for um array, pega apenas os primeiros 5 elementos e transforma-os em uma string separada por vírgulas
+    if (Array.isArray(numeros)) {
+      numeros = numeros.slice(0, 5).join(',');
     }
-  });
+    // Agora, 'numeros' é uma string e pode ser usada para criar a nova aposta
+    await Apostas.create({ userName, userCPF, numeros });
+    console.log('Esses são os números', numeros);
+    console.log(req.session.userCPF, req.session.userName, numeros)
+  res.render('sucesso')
+  } catch (error) {
+    console.log(error); 
+    res.send(error);
+  }
+});
+
 
 module.exports = router;
